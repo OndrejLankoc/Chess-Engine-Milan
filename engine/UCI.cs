8f80d434aea6ab97e5d4c4c9a003309b5842d167
spsOndrejLankoc <lankocondrej@sps-karvina.cz>
2025-10-17 18:21:32 +0200

    Add UCI interface implementation

---
+++ engine/UCI.cs
@@ -0,0 +1,76 @@
+namespace Engine
+{
+    public class Uci
+    {
+        private Board board = new Board();
+        private Engine engine = new Engine();
+
+        public List<Move> PlayedMoves = new List<Move>();
+        public List<MoveInfo> PlayedMovesInfo = new List<MoveInfo>();
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "") continue;
+
+                switch (input)
+                {
+                    case "uci":
+                        Console.WriteLine("id name Engine");
+                        Console.WriteLine("id author OL");
+                        Console.WriteLine("uciok");
+                        break;
+
+                    case "isready":
+                        Console.WriteLine("readyok");
+                        break;
+
+                    case string s when s.StartsWith("position"):
+                        HandlePosition(s);
+                        break;
+
+                    case string s when s.StartsWith("go"):
+                        engine.Search(board, 4, out Move bestMove, PlayedMoves, PlayedMovesInfo);
+
+                        PlayedMovesInfo.Add(board.MakeMove(bestMove));
+                        PlayedMoves.Add(bestMove);
+                        engine.TT.ClearOldEntries(board.halfMoveClock);
+
+                        Console.WriteLine($"bestmove {bestMove.ToString()}");
+                        break;
+
+                    case "quit":
+                        return;
+                }                
+            }
+        }
+
+        private void HandlePosition(string input)
+        {
+            if (input.Contains("startpos"))
+            {
+                board.SetupBoard();
+
+                if (input.Contains("moves"))
+                {
+                    string moves = input.Split("moves ")[1];
+                    foreach (string move in moves.Split(' '))
+                    {
+                        Move.TryParse(board.sideToMove, move, out Move parsedMove);
+                        PlayedMovesInfo.Add(board.MakeMove(parsedMove));
+                        PlayedMoves.Add(parsedMove);
+                    }
+                }
+            }
+
+            else if (input.Contains("fen"))
+            {
+                string fen = input.Split("fen ")[1];
+                board.SetupBoard(fen);
+            }
+        }
+    }
+}
